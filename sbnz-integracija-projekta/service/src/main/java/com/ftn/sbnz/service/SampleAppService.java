07fdf8ff6c3f7dd66f82b2a097806e7143423109
bob <bob@bob.bob>
2025-09-25 15:39:01 +0200

    added template

--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/SampleAppService.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/SampleAppService.java
@@ -15,18 +15,27 @@ import java.util.*;
 public class SampleAppService {
 
 	private final KieSession fwSession;
+    private final KieSession tempSession;
 
 	@Autowired
-	public SampleAppService(@Qualifier("fwKsession") KieSession fwSession) {
+	public SampleAppService(@Qualifier("fwKsession") KieSession fwSession, @Qualifier("tempKsession") KieSession tempSession) {
 		this.fwSession = fwSession;
+        this.tempSession = tempSession;
 
 		// Dodaj listener za sve sesije odmah pri kreiranju servisa
 		this.fwSession.addEventListener(new DefaultAgendaEventListener() {
 			@Override
 			public void afterMatchFired(AfterMatchFiredEvent event) {
-				System.out.println("Pravilo aktivirano: " + event.getMatch().getRule().getName());
+				System.out.println("FW Pravilo aktivirano: " + event.getMatch().getRule().getName());
 			}
 		});
+        this.tempSession.addEventListener(new DefaultAgendaEventListener() {
+            @Override
+            public void afterMatchFired(AfterMatchFiredEvent event) {
+                System.out.println("Temp Pravilo aktivirano: " + event.getMatch().getRule().getName());
+            }
+        });
+
 	}
 
 	/**
@@ -59,22 +68,36 @@ public class SampleAppService {
 	 */
 	public Map<String, Object> insertServiceAndTrack(NetworkService service) {
 		Map<String, Object> response = new HashMap<>();
-		List<String> firedRules = new ArrayList<>();
+		List<String> firedRulesFW = new ArrayList<>();
+        List<String> firedRulesTemp = new ArrayList<>();
 
 		fwSession.addEventListener(new DefaultAgendaEventListener() {
 			@Override
 			public void afterMatchFired(AfterMatchFiredEvent event) {
-				firedRules.add(event.getMatch().getRule().getName());
+                firedRulesFW.add(event.getMatch().getRule().getName());
 			}
 		});
+        tempSession.addEventListener(new DefaultAgendaEventListener() {
+            @Override
+            public void afterMatchFired(AfterMatchFiredEvent event) {
+                firedRulesTemp.add(event.getMatch().getRule().getName());
+            }
+        });
 
 		fwSession.insert(service);
-		int count = fwSession.fireAllRules();
-
-		response.put("count: ",count);
-		response.put("firedRules", firedRules);
-		response.put("sessionObjects", fwSession.getObjects());
-
+		int countFW = fwSession.fireAllRules();
+        tempSession.insert(service);
+        int countTemp = tempSession.fireAllRules();
+        if(countFW != 0) {
+            response.put("countFW: ", countFW);
+            response.put("firedFWRules", firedRulesFW);
+            response.put("FWsessionObjects", fwSession.getObjects());
+        }
+        if(countTemp != 0) {
+            response.put("countTemp: ", countTemp);
+            response.put("firedTempRules", firedRulesTemp);
+            response.put("TempsessionObjects", tempSession.getObjects());
+        }
 		return response;
 	}
 
--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/config/DroolsConfig.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/config/DroolsConfig.java
@@ -1,10 +1,18 @@
 package com.ftn.sbnz.service.config;
 
+import org.kie.api.builder.Message;
+import org.kie.api.builder.Results;
+import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.internal.utils.KieHelper;
+import org.drools.decisiontable.ExternalSpreadsheetCompiler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.InputStream;
+import java.util.List;
 
 @Configuration
 public class DroolsConfig {
@@ -25,5 +33,40 @@ public class DroolsConfig {
     @Bean
     public KieSession cepKsession() {
         return kieContainer.newKieSession("cepKsession");
+    }
+
+
+    @Bean
+    public KieSession tempKsession() {
+
+
+        InputStream template = DroolsConfig.class.getResourceAsStream("/templatetable/template.drt");
+        InputStream data = DroolsConfig.class.getResourceAsStream("/templatetable/template-data.xls");
+
+        ExternalSpreadsheetCompiler converter = new ExternalSpreadsheetCompiler();
+        String drl = converter.compile(data, template, 3, 2);
+
+        System.out.println(drl);
+
+        return this.createKieSessionFromDRL(drl);
+
+    }
+
+    private KieSession createKieSessionFromDRL(String drl){
+        KieHelper kieHelper = new KieHelper();
+        kieHelper.addContent(drl, ResourceType.DRL);
+
+        Results results = kieHelper.verify();
+
+        if (results.hasMessages(Message.Level.WARNING, Message.Level.ERROR)){
+            List<Message> messages = results.getMessages(Message.Level.WARNING, Message.Level.ERROR);
+            for (Message message : messages) {
+                System.out.println("Error: "+message.getText());
+            }
+
+            throw new IllegalStateException("Compilation errors were found. Check the logs.");
+        }
+
+        return kieHelper.build().newKieSession();
     }
 }
